ca51a4c38000f6a908456deff808399100afc5a1
Pai, Darshan S <darshan.pai@ge.com>
2019-11-14 16:46:19 -0600

    More problems

--- Python_DCP/DIP_Nov13/maxinstack.py
+++ Python_DCP/DIP_Nov13/maxinstack.py
@@ -34,15 +34,3 @@ class MaxStack:
         return self.maxstack[-1]
 
     return None
-
-s = MaxStack()
-s.push(1)
-s.push(2)
-s.push(3)
-s.push(2)
-print(s.max())
-# 3
-s.pop()
-s.pop()
-print(s.max())
-# 2
